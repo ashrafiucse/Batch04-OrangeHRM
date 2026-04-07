57f188c2f9bff61bd58e3a6b4b4290cd1145a811
Ashraf Ali <ashraf.iu.cse1718@gmail.com>
2026-04-07 23:08:08 +0600

    added login further tests and PIM

--- cypress/e2e/Login/login.spec.cy.js
+++ cypress/e2e/Login/login.spec.cy.js
@@ -1,7 +1,9 @@
 describe('Login Related Testcases', () => {
+  beforeEach(()=> {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+  })
 
   it('should login with valid credentials', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/');
     //cy.get('input[name="username"]').type("Admin");
     cy.get('[name="username"]').type("Admin");
     cy.get('input[name="password"]').type('admin123');
@@ -11,12 +13,28 @@ describe('Login Related Testcases', () => {
   });
 
   it('should not login with invalid credentials', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/');
     //cy.get('input[name="username"]').type("Admin");
     cy.get('[name="username"]').type("Admin");
     cy.get('input[name="password"]').type('admin1234');
     cy.get('.orangehrm-login-button').click();
     cy.contains('Invalid credentials').should('be.visible');
+  });
+
+  it('validation message should be appeared when password field leave empty and username is filled out with valid username', () => {
+    //cy.get('input[name="username"]').type("Admin");
+    cy.get('[name="username"]').type("Admin");
+    cy.get('.orangehrm-login-button').click();
+    cy.contains('Required').should('be.visible');
+  });
+
+  it('validation message should be appeared when username and password are empty', () => {
+    //cy.get('input[name="username"]').type("Admin");
+    cy.get('.orangehrm-login-button').click();
+
+    cy.get('.oxd-input-field-error-message').eq(0).should('contain','Required');
+    cy.get('.oxd-input-field-error-message').eq(1).should('contain','Required');
+    // cy.contains('Required').eq(0).should('be.visible');
+    // cy.contains('Required').eq(1).should('be.visible');
   });
 
 });
